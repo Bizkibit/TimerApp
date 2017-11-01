22cf1c3b7fdddf8493e4662065d10b9443ebe88a
Farshid HM <fhmanesh@gmail.com>
2017-11-01 00:17:35 -0700

    fixing config js file

--- models/time.js
+++ models/time.js
@@ -1,13 +1,13 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   const Time = sequelize.define('Time', {
     current_time: DataTypes.FLOAT
-  }/*, {
+  /*, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
       }
     }
-  }*/);
+  }*/});
   return Time;
 };
